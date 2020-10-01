fd157dfa5f53d2eae94ab41e3d86107592d6d41c
AlenaChupina <chupina.elen@gmail.com>
2020-10-01 10:14:20 -0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+if (!array || array.length == 0){return 0};
+  const sortedArr = [...array].sort((a,b)=>a-b);
+  return sortedArr[0];
+  }
 
 exports.max = function max (array) {
-  return 0;
+   if (!array || array.length == 0){return 0};
+   const sortedArr = [...array].sort((a,b)=>b-a);
+    return  sortedArr[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+if (!array || array.length == 0){return 0};
+ const reducedArray = [...array].reduce((acc,val)=>{
+    return acc+val;  
+  },0)
+  const avgRes = reducedArray/array.length;
+  return  avgRes;
+ };
+ 
